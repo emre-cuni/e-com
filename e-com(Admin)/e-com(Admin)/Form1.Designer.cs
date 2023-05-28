61c6f3dac84f95cdd2c832c9a2698c320d08c7e5
Emre Cüni <emrecuni0067@gmail.com>
2023-05-28 19:12:36 +0300

    e-com

---
+++ e-com(Admin)/e-com(Admin)/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace e_com_Admin_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxProductName = new System.Windows.Forms.TextBox();
+            this.textBoxProductBrand = new System.Windows.Forms.TextBox();
+            this.textBoxProductDescription = new System.Windows.Forms.TextBox();
+            this.textBoxProductSubCategory = new System.Windows.Forms.TextBox();
+            this.textBoxProductSupCategory = new System.Windows.Forms.TextBox();
+            this.buttonSelectImage = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonRegisterProduct = new System.Windows.Forms.Button();
+            this.textBoxProductPrice = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxProductName
+            // 
+            this.textBoxProductName.Location = new System.Drawing.Point(114, 20);
+            this.textBoxProductName.Name = "textBoxProductName";
+            this.textBoxProductName.Size = new System.Drawing.Size(165, 20);
+            this.textBoxProductName.TabIndex = 0;
+            // 
+            // textBoxProductBrand
+            // 
+            this.textBoxProductBrand.Location = new System.Drawing.Point(114, 47);
+            this.textBoxProductBrand.Name = "textBoxProductBrand";
+            this.textBoxProductBrand.Size = new System.Drawing.Size(165, 20);
+            this.textBoxProductBrand.TabIndex = 1;
+            // 
+            // textBoxProductDescription
+            // 
+            this.textBoxProductDescription.Location = new System.Drawing.Point(114, 74);
+            this.textBoxProductDescription.Name = "textBoxProductDescription";
+            this.textBoxProductDescription.Size = new System.Drawing.Size(165, 20);
+            this.textBoxProductDescription.TabIndex = 2;
+            // 
+            // textBoxProductSubCategory
+            // 
+            this.textBoxProductSubCategory.Location = new System.Drawing.Point(114, 101);
+            this.textBoxProductSubCategory.Name = "textBoxProductSubCategory";
+            this.textBoxProductSubCategory.Size = new System.Drawing.Size(165, 20);
+            this.textBoxProductSubCategory.TabIndex = 3;
+            // 
+            // textBoxProductSupCategory
+            // 
+            this.textBoxProductSupCategory.Location = new System.Drawing.Point(114, 128);
+            this.textBoxProductSupCategory.Name = "textBoxProductSupCategory";
+            this.textBoxProductSupCategory.Size = new System.Drawing.Size(165, 20);
+            this.textBoxProductSupCategory.TabIndex = 4;
+            // 
+            // buttonSelectImage
+            // 
+            this.buttonSelectImage.Location = new System.Drawing.Point(285, 181);
+            this.buttonSelectImage.Name = "buttonSelectImage";
+            this.buttonSelectImage.Size = new System.Drawing.Size(165, 34);
+            this.buttonSelectImage.TabIndex = 6;
+            this.buttonSelectImage.Text = "Ürün Fotoğrafı Seç";
+            this.buttonSelectImage.UseVisualStyleBackColor = true;
+            this.buttonSelectImage.Click += new System.EventHandler(this.buttonSelectImage_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(46, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ürün Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(24, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Ürün Markası:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(11, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Ürün Açıklaması:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(33, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Alt Kategori:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(29, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Üst Kategori:";
+            // 
+            // buttonRegisterProduct
+            // 
+            this.buttonRegisterProduct.Location = new System.Drawing.Point(114, 181);
+            this.buttonRegisterProduct.Name = "buttonRegisterProduct";
+            this.buttonRegisterProduct.Size = new System.Drawing.Size(165, 33);
+            this.buttonRegisterProduct.TabIndex = 7;
+            this.buttonRegisterProduct.Text = "Ürünü Kaydet";
+            this.buttonRegisterProduct.UseVisualStyleBackColor = true;
+            this.buttonRegisterProduct.Click += new System.EventHandler(this.buttonRegisterProduct_Click);
+            // 
+            // textBoxProductPrice
+            // 
+            this.textBoxProductPrice.Location = new System.Drawing.Point(114, 155);
+            this.textBoxProductPrice.Name = "textBoxProductPrice";
+            this.textBoxProductPrice.Size = new System.Drawing.Size(165, 20);
+            this.textBoxProductPrice.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(37, 161);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Ürün Fiyatı:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(285, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(165, 154);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(46)))), ((int)(((byte)(46)))));
+            this.ClientSize = new System.Drawing.Size(479, 242);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxProductPrice);
+            this.Controls.Add(this.buttonRegisterProduct);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSelectImage);
+            this.Controls.Add(this.textBoxProductSupCategory);
+            this.Controls.Add(this.textBoxProductSubCategory);
+            this.Controls.Add(this.textBoxProductDescription);
+            this.Controls.Add(this.textBoxProductBrand);
+            this.Controls.Add(this.textBoxProductName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxProductName;
+        private System.Windows.Forms.TextBox textBoxProductBrand;
+        private System.Windows.Forms.TextBox textBoxProductDescription;
+        private System.Windows.Forms.TextBox textBoxProductSubCategory;
+        private System.Windows.Forms.TextBox textBoxProductSupCategory;
+        private System.Windows.Forms.Button buttonSelectImage;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonRegisterProduct;
+        private System.Windows.Forms.TextBox textBoxProductPrice;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ e-com(Admin)/e-com(Admin)/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace e_com_Admin_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static string connectionString = "Server=.;Database=e-com;User Id=Cuni;Password=123456;";
+        StringBuilder productName = new StringBuilder();
+        StringBuilder productBrand = new StringBuilder();
+        StringBuilder productDescription = new StringBuilder();
+        StringBuilder productSupCategory = new StringBuilder();
+        StringBuilder productSubCategory = new StringBuilder();
+        StringBuilder productPrice = new StringBuilder();
+        StringBuilder imagePath = new StringBuilder();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSelectImage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.Title = "Resim Seç";
+                openFileDialog1.Filter = "Jpeg Dosyaları(*.jpeg)|*.jpeg|Jpg Dosyaları(*.jpg)|*.jpg|Png Dosyaları(*.png)|*.png";
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    imagePath.Append(openFileDialog1.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ex.message: " + ex.Message + " stacktrace: " + ex.StackTrace, "Resim Seç Hatası", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonRegisterProduct_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Ürün bilgileri alınır
+                productName.Append(textBoxProductName.Text);
+                productBrand.Append(textBoxProductBrand.Text);
+                productDescription.Append(textBoxProductDescription.Text);
+                productSupCategory.Append(textBoxProductSupCategory.Text);
+                productSubCategory.Append(textBoxProductSubCategory.Text);                
+                productPrice.Append(textBoxProductPrice.Text);
+
+                //seçilen fotoğraf byte array'e çevrilir
+                FileStream fileStream = new FileStream(imagePath.ToString(), FileMode.Open, FileAccess.Read);
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                byte[] image = binaryReader.ReadBytes((int)fileStream.Length);
+                binaryReader.Close();
+                fileStream.Close();
+
+                //Sql Bağlantısı Açılır
+                SqlConnection sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+
+                //kayıt sorgusu oluşturulur
+                SqlCommand command = new SqlCommand("Insert Into Table_Product(productName, productBrand, productDescription, productSupCategory, productSubCategory, productPrice, productPhoto) Values(@name, @brand, @description, @supCategory, @subCategory, @price, @photo)", sqlConnection);
+                command.Parameters.Add("@name",SqlDbType.NVarChar, productName.Length).Value = productName.ToString();
+                command.Parameters.Add("@brand", SqlDbType.NVarChar, productBrand.Length).Value = productBrand.ToString();
+                command.Parameters.Add("@description", SqlDbType.NVarChar, productDescription.Length).Value = productDescription.ToString();
+                command.Parameters.Add("@supCategory", SqlDbType.NVarChar, productSupCategory.Length).Value = productSupCategory.ToString();
+                command.Parameters.Add("@subCategory", SqlDbType.NVarChar, productSubCategory.Length).Value = productSubCategory.ToString();
+                command.Parameters.Add("@price", SqlDbType.NVarChar, productPrice.Length).Value = productPrice.ToString();
+                command.Parameters.Add("@photo", SqlDbType.Image, image.Length).Value = image;
+                command.ExecuteNonQuery();
+                sqlConnection.Close();
+                MessageBox.Show("Kayıt Eklendi", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                //değişkenler temizlenir
+                productName.Clear();
+                productBrand.Clear();
+                productDescription.Clear();
+                productSupCategory.Clear();
+                productSubCategory.Clear();
+                productPrice.Clear();
+                imagePath.Clear();
+                pictureBox1.Image = null;
+                textBoxProductName.Text = string.Empty;
+                textBoxProductBrand.Text = string.Empty;
+                textBoxProductDescription.Text = string.Empty;
+                textBoxProductSupCategory.Text = string.Empty;
+                textBoxProductSubCategory.Text = string.Empty;
+                textBoxProductPrice.Text = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ex.message: " + ex.Message + " stacktrace: " + ex.StackTrace, "Kayıt Hatası", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }            
+        }
+    }
+}
